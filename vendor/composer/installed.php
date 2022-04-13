5c1e13c03d2a052a16c3f2568cce38e68b085eeb
Andreas Behrens <55226648+AndreasBehrensLG@users.noreply.github.com>
2022-04-13 16:17:01 +0200

    First commit
    My first Composer project to register the vendorname "secdevab"

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dac44d5709f934e870e32f17968567813053d3bd',
+        'name' => 'secdevab/hello-world',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'secdevab/hello-world' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dac44d5709f934e870e32f17968567813053d3bd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
